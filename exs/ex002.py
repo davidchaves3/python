f79a04ee4ed1d10d430699cd27fdbcf351a1351b
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-08-05 16:15:50 -0300

    aula

---
+++ exs/ex002.py
@@ -0,0 +1,8 @@
+print('====== DESAFIO 02 ======')
+
+dia = input('Dia = ')
+mes = input('Mes = ')
+ano = input('Ano = ')
+
+print('Você nasceu no dia ' + dia + ' de ' + mes + ' de ' + ano + ' . Correto?')
+      
---
+++ exs/ex003.py
@@ -0,0 +1,6 @@
+print(' ======= Desafio 03 ===== ')
+
+num1 = int( input('Primeiro número'))
+num2 = int( input('Segundo número'))
+
+print('A soma é ', num1 + num2)
