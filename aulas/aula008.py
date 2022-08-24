6fe930954c59c1566e5877344deb99fda02d2140
David Chaves <109917681+davidchaves3@users.noreply.github.com>
2022-08-24 15:32:02 -0300

    aula

---
+++ aulas/aula008.py
@@ -0,0 +1,6 @@
+import math
+num =  int(input('Digite um número: '))
+
+raiz = math.sqrt(num)
+
+print('A raiz é {}'.format(math.ceil(raiz))) 
---
+++ aulas/teste.py
@@ -0,0 +1,12 @@
+num1 = float(input('Digite um número:'))
+num2 = float(input('Digite um número:'))
+
+soma = num1 + num2
+
+if soma % 2 == 0:
+
+    print('é um número par')
+
+else:
+
+    print('Impar')
